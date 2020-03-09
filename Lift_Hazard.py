f3e74355863245893481ab1c5a5b4be563c36043
Ritam Majumder <61985714+ritam3144@users.noreply.github.com>
2020-03-09 23:13:20 +0530

    Create Lift_Hazard.py

---
+++ Lift_Hazard.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 12:27:55 2020
+
+@author: RitamM
+"""
+import pandas as pd
+import sys
+position_of_lifts = input("Enter the position of 3 lifts 1st lift,2nd lift,3rd lift => ").split(',')
+print("\n Max number of registered call is 3, once a call is"+ 
+      " registered to a lift, it discards all the next call and servs the first 3 calls.\n"+
+      "So the length of a calls list will be 3.")
+calls = input("Enter the floors where lift is called 1st Call,2nd Call,3rd Call => ").split(',')
+if(len(calls) > 3):
+    sys.exit("Read the condition again :) \n")
+position_of_lifts = [int(i) for i in position_of_lifts]
+calls = [int(i) for i in calls]
+dist = []
+lifts = []
+for i in calls:
+    for j in position_of_lifts:
+        dist.append(abs(i - j))
+    lifts.append(dist)
+    dist = []
+df = pd.DataFrame(lifts)
+for j in range(len(df)):
+    mindf = df.min(axis = 1).sort_values(ascending = False)
+    key,value = zip(*mindf.iteritems())
+    cols = df.loc[:,(df==value[0]).any()].columns.values[0]
+    index = df[df[cols] == value[0]].index.values[0]
+    position_of_lifts[index] = calls[cols]
+    df = df.drop(cols,axis = 1)
+    df = df[df.index != index]
+print("Final Position of lifts =>\n",position_of_lifts)
